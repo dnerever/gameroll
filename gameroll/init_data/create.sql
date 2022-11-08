6b0dab0e54fc82b81a75006459c22e879ee31ce0
kevinjmcmahon <kemc1117@colorado.edu>
2022-11-08 14:03:50 -0700

    users_to_games & games table created

--- gameroll/init_data/create.sql
+++ gameroll/init_data/create.sql
@@ -1,12 +1,19 @@
-
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE game2users(
-user_id SERIAL FOREIGN KEY,
-game_id SERIAL FOREIGN KEY,
+DROP TABLE IF EXISTS users_to_games CASCADE;
+CREATE TABLE users_to_games(
+  user_id INT NOT NULL,
+  game_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  FOREIGN KEY (game_id) REFERENCES games_to_api (game_id)
+);
+
+DROP TABLE IF EXISTS games CASCADE;
+CREATE TABLE games(
+    game_id PRIMARY KEY
 );
