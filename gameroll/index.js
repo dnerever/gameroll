a309b68959dbc2f388c02761c2958fe7173f02b8
dnerever <keba6138@colorado.edu>
2022-11-17 13:46:52 -0700

    Adding if statement to /savegame

--- gameroll/index.js
+++ gameroll/index.js
@@ -247,38 +247,39 @@ app.get('/nextGame', (req,res) => {
 });
 
 app.get('/saveGame', (req,res) => {
-    console.log(games.data);
+  console.log(games.data);
+  if ()
 });
 
 // We don't want this because we want users to be able to see our website without having to log in 
-  // // Authentication Required
-  app.use(auth);
+// // Authentication Required
+app.use(auth);
 
-  app.get('/profile', (req, res) => {
-    axios({
-      url: "https://api.igdb.com/v4/games",
-      method: 'POST',
-      dataType: 'text',
-      headers: {
-          
-          "Client-ID": " 5nphybqacwmj6kh3m2m0hk3unjc1gn",
-          "Authorization": "Bearer fewdbr1edvvqbiughfqnu7z0ibl0bj",
-      },
-      data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; limit 1",
-      body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; limit 1",
-    })
-      .then(games => {
-          console.log(games.data);
-        res.render('pages/profile', {games: data})
-        })
-      .catch(err => {
-          res.render('pages/profile',{
-            games: [],
-            message: err.message || err
-          });
-          
-      });
-  });
+app.get('/profile', (req, res) => {
+  axios({
+    url: "https://api.igdb.com/v4/games",
+    method: 'POST',
+    dataType: 'text',
+    headers: {
+        
+        "Client-ID": " 5nphybqacwmj6kh3m2m0hk3unjc1gn",
+        "Authorization": "Bearer fewdbr1edvvqbiughfqnu7z0ibl0bj",
+    },
+    data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; limit 1",
+    body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; limit 1",
+  })
+    .then(games => {
+        console.log(games.data);
+      res.render('pages/profile', {games: data})
+      })
+    .catch(err => {
+        res.render('pages/profile',{
+          games: [],
+          message: err.message || err
+        });
+        
+    });
+});
   
   
 // 11. GET /logout
